eb1a5c6efdef1295d7cde95faa3bc5379b97cd76
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-09 18:02:11 +0530

    Write controller logic

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Hash;
+use DB;
+use App\Models\User;
+use App\Http\Requests\FormPostRequest;
 
 class FormController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function store(FormPostRequest $request)
+    {
+        DB::beginTransaction();
+        try {
+            $data = [
+                'name' => $request->name,
+                'email' => $request->email,
+                'username' => $request->username,
+                'password' => Hash::make($request->password),
+                'phone_number' => $request->phone_number
+            ];
+
+            User::create($data);
+            DB::commit();
+            return redirect()->back()->with('success', 'User created successfully');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+    }
 }
